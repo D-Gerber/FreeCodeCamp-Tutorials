c9b9238bc09d8e5ef47dd0a16291c3d7b90608e3
Daniel <drgerber69@gmail.com>
2024-05-07 12:38:43 +0200

    Update Script.js

--- Learn Modern JavaScript Methods by Building Football Team Card/Script.js
+++ Learn Modern JavaScript Methods by Building Football Team Card/Script.js
@@ -9,4 +9,12 @@ const myFavoriteFootballTeam = {
     team: "Argentina",
     sport: "Football",
     year: 1986,
+    isWorldCupWinner: true,
+    headCoach: {
+        coachName: "Carlos Bilardo",
+        matches: 7,
+    },
+    players: [
+
+    ]
 };
