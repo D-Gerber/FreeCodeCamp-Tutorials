f5e6129f9b0e67c7d002d9130f47c82e9aeb07d8
Daniel <drgerber69@gmail.com>
2024-05-07 14:44:41 +0200

    Update Script.js

--- Learn Modern JavaScript Methods by Building Football Team Card/Script.js
+++ Learn Modern JavaScript Methods by Building Football Team Card/Script.js
@@ -180,3 +180,20 @@ typeOfSport.textContent = sport;
 teamName.textContent = team;
 worldCupYear.textContent = year;
 headCoach.textContent = coachName;
+
+const setPlayerCards = (arr = players) => {
+
+
+    playerCards.innerHTML += arr.map(
+        ({ name, position, number, isCaptain, nickname }) => {
+            `
+          <div class="player-card">
+        
+          </div>
+        `;
+        }
+    );
+
+
+};
+
