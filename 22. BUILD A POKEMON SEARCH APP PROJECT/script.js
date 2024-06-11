a5fc45b5476a49f18c16428403bf332dec01ea9b
Daniel <drgerber69@gmail.com>
2024-06-11 13:47:39 +0200

    Update script.js

--- 22. BUILD A POKEMON SEARCH APP PROJECT/script.js
+++ 22. BUILD A POKEMON SEARCH APP PROJECT/script.js
@@ -12,3 +12,48 @@ const specialAttack = document.getElementById("special-attack");
 const specialDefense = document.getElementById("special-defense");
 const speed = document.getElementById("speed");
 const spriteContainer = document.getElementById("sprite-container");
+
+const getPokemon = async () => {
+    try {
+        const pokemonNameOrId = searchInput.value.toLowerCase();
+        const res = await fetch(`https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/${pokemonNameOrId}`);
+        const data = await res.json();
+        setPokemonInfo(data);
+    } catch (err) {
+        alert("Pokémon not found")
+        console.log(err);
+    }
+};
+
+const setPokemonInfo = data => {
+    const { name, id, weight, height, types, sprites, stats } = data;
+
+    pokemonName.textContent = `${name[0].toUpperCase() + name.slice(1)}`;
+    pokemonId.textContent = `#${id}`;
+    pokemonWeight.textContent = `Weight: ${weight}`;
+    pokemonHeight.textContent = `Height: ${height}`;
+
+    spriteContainer.innerHTML = `<img id="sprite" src="${sprites.front_default}" alt="${name}">`;
+
+    hp.textContent = stats[0].base_stat;
+    attack.textContent = stats[1].base_stat;
+    defense.textContent = stats[2].base_stat;
+    specialAttack.textContent = stats[3].base_stat;
+    specialDefense.textContent = stats[4].base_stat;
+    speed.textContent = stats[5].base_stat;
+
+    pokemonTypes.innerHTML = types.map((obj) => `
+    <span>${obj.type.name[0].toUpperCase() + obj.type.name.slice(1)}</span>
+  `).join(" ");
+};
+
+searchButton.addEventListener("click", e => {
+    e.preventDefault();
+    getPokemon()
+});
+
+searchInput.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        searchButton.click();
+    }
+});
